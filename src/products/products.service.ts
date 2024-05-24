353a6ad299070f032171ecd932269ffd24057ffe
claudiocoder <clauartur98@gmail.com>
2024-05-24 17:51:20 -0600

    feat: add available field into Products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -24,13 +24,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({ where: { available: true } });
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: { available: true },
       }),
       meta: {
         total: totalPages,
@@ -41,7 +42,9 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   async findOne(id: number) {
-    const product = await this.product.findFirst({ where: { id } });
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
@@ -55,6 +58,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async remove(id: number) {
     await this.findOne(id);
-    return this.product.delete({ where: { id } });
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+    return product;
   }
 }
