f1e6bf8801ea61dfe69a9a983940a76c623bc6d8
claudiocoder <clauartur98@gmail.com>
2024-05-24 17:25:40 -0600

    feat: add Hard delete product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -53,7 +53,8 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     return this.product.update({ where: { id }, data: updateProductDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.product.delete({ where: { id } });
   }
 }
