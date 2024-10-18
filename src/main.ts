c8f3a2514ed8a9feca580dac99d751ba36dfad19
clcoder <clauartur98@gmail.com>
2024-10-18 17:52:22 -0600

    :sparkles: - Adds NATS transport service

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const logger = new Logger('main');
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    { transport: Transport.TCP, options: { port: envs.port } },
+    {
+      transport: Transport.NATS,
+      options: { servers: envs.natsServers },
+    },
   );
   app.useGlobalPipes(
     new ValidationPipe({
